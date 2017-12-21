8b7e11fdb36aa80ef0623ed7023be5bc87af0144
LeeSungOn <dltjddhs@gmail.com>
2017-12-21 12:59:38 +0900

    add code

--- src/codeground/Template.java
+++ src/codeground/Template.java
@@ -1,5 +1,29 @@
 //package codeground;
-//
+
+/*
+* 문제: 1 / SCPC 연습문제 숫자 골라내기
+* link: https://www.codeground.org/practice
+* 알고리즘: 구현
+* 풀이방법:
+*   입력받은 숫자 중 홀수 번으로 입력받은 숫자에 대한 xor 연산 결과를 출력한다.
+*   짝수번으로 입력받은 숫자에 대해서 xor 연산을 수행 하면 해당 숫자는 상쇄되는 성질을 이용한다.
+* 의사코드(Pseudo Code)
+*   input T
+*   each T
+*       answer <- 0
+*       input N
+*       for i: 0 to N-1
+*           input number
+*           answer <- answer xor number*
+*
+* 시간복잡도(Time Complexity)
+*   각 TestCase T에 대하여 N개의 숫자를 입력받고 연산을 수행하므로 O(TN)
+*
+* 공간복잡도(Space Complexity)
+*   자료구조를 사용하지 않음. O(1)
+*
+* */
+
 ///*
 //You should use the statndard input/output
 //
