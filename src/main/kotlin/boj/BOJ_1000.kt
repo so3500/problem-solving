528621da4b94153162b0da711f7f7a97a490334f
so3500 <dltjddhs@gmail.com>
2019-08-23 01:05:07 +0900

    BOJ_1000: A + B

---
+++ src/main/kotlin/boj/BOJ_1000.kt
@@ -0,0 +1,12 @@
+package boj
+
+import java.util.*
+
+fun main() {
+    val reader = Scanner(System.`in`)
+
+    val a: Int = reader.nextInt();
+    val b: Int = reader.nextInt();
+
+    print("${a + b}")
+}
