b25bc006f69f9695d6ffbaa7071e4f5908ef1d95
so3500 <dltjddhs@gmail.com>
2019-10-21 20:48:13 +0900

    [LC-26] RemoveDuplicatedFromSortedArray

--- src/main/java/leetcode/LC_26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/leetcode/LC_26_RemoveDuplicatesFromSortedArray.java
@@ -1,9 +1,24 @@
-/*
- * Copyright (c) 2019 LINE Corporation. All rights reserved.
- * LINE Corporation PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
 package leetcode;
 
 public class LC_26_RemoveDuplicatesFromSortedArray {
+
+	public int removeDuplicates(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		int minNum = nums[0];
+		int distinctCount = 1;
+		int leftIdx = 0;
+
+		for (int num : nums) {
+			if (minNum < num) {
+				leftIdx++;
+				nums[leftIdx] = num;
+				distinctCount++;
+				minNum = num;
+			}
+		}
+		return distinctCount;
+	}
 }
