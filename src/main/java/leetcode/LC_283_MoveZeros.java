c0d6f96426c21de69d41dc082c59b4f7fcb83202
so3500 <dltjddhs@gmail.com>
2021-09-20 19:18:16 +0900

    [leetcode] 283. MoveZeros

--- src/main/java/leetcode/LC_283_MoveZeros.java
+++ src/main/java/leetcode/LC_283_MoveZeros.java
@@ -4,15 +4,59 @@ import java.util.Arrays;
 
 public class LC_283_MoveZeros {
 
-    public void moveZeroes(int[] nums) {
-        int position = 0;
-        for (int num : nums) {
-            if (num != 0) {
-                nums[position] = num;
-                position++;
-            }
-        }
+	public void moveZeroes(int[] nums) {
+		moveZeroes1(nums);
+		//moveZeroes2(nums);
+	}
 
-        Arrays.fill(nums, position, nums.length, 0);
-    }
+	/**
+	 * Time complexity: O(N)
+	 * Space complexity: O(1) in-place
+	 */
+	public void moveZeroes1(int[] nums) {
+		int position = 0;
+		for (int num : nums) {
+			if (num != 0) {
+				nums[position] = num;
+				position++;
+			}
+		}
+
+		Arrays.fill(nums, position, nums.length, 0);
+	}
+
+	/**
+	 * Time complexity: O(N)
+	 * Space complexity: O(1) in-place
+	 */
+	public void moveZeroes2(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				int nonZeroIndex = findNonZeroIndex(nums, i + 1);
+
+				if (nonZeroIndex == nums.length) {
+					break;
+				}
+
+				swap(nums, i, nonZeroIndex);
+			}
+		}
+	}
+
+	private int findNonZeroIndex(int[] nums, int start) {
+		int nonZeroIndex = start;
+		while (nonZeroIndex < nums.length) {
+			if (nums[nonZeroIndex] != 0) {
+				break;
+			}
+			nonZeroIndex++;
+		}
+		return nonZeroIndex;
+	}
+
+	private void swap(int[] nums, int a, int b) {
+		int temp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = temp;
+	}
 }
--- src/test/java/leetcode/LC_283_MoveZerosTest.java
+++ src/test/java/leetcode/LC_283_MoveZerosTest.java
@@ -1,11 +1,11 @@
 package leetcode;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class LC_283_MoveZerosTest {
+class LC_283_MoveZerosTest {
     private LC_283_MoveZeros lc283;
 
     @BeforeEach
@@ -15,12 +15,16 @@ public class LC_283_MoveZerosTest {
 
     @Test
     void moveZeros() {
-        final int[] tc1 = { 0, 1, 0, 3, 12 };
+        final int[] tc1 = {0, 1, 0, 3, 12};
         lc283.moveZeroes(tc1);
-        assertThat(tc1).isEqualTo(new int[] { 1, 3, 12, 0, 0 });
+        assertThat(tc1).isEqualTo(new int[] {1, 3, 12, 0, 0});
 
-        final int[] tc2 = { 0, 0, 0, 3, 12, 123 };
+        final int[] tc2 = {0, 0, 0, 3, 12, 123};
         lc283.moveZeroes(tc2);
-        assertThat(tc2).isEqualTo(new int[] { 3, 12, 123, 0, 0, 0 });
+        assertThat(tc2).isEqualTo(new int[] {3, 12, 123, 0, 0, 0});
+
+        final int[] tc3 = {1, 2};
+        lc283.moveZeroes(tc3);
+        assertThat(tc3).isEqualTo(new int[] {1, 2});
     }
 }
