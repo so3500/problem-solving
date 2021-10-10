12ae27c2dec11dbaed868220a9efa83b14210f62
so3500 <dltjddhs@gmail.com>
2021-10-10 17:35:51 +0900

    [leetcode] 21. MergeTwoSortedLists

--- src/main/java/leetcode/LC_21_MergeTwoSortedLists.java
+++ src/main/java/leetcode/LC_21_MergeTwoSortedLists.java
@@ -12,6 +12,39 @@ package leetcode;
 public class LC_21_MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return mergeTowListsWithInPlace(l1, l2);
+        //		return mergeTowListsWithAllocation(l1, l2);
+    }
+
+    /**
+     * Time complexity O(N)
+     * Space complexity O(1)
+     */
+    private ListNode mergeTowListsWithInPlace(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cursor = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cursor.next = l1;
+                l1 = l1.next;
+            } else {
+                cursor.next = l2;
+                l2 = l2.next;
+            }
+            cursor = cursor.next;
+        }
+
+        cursor.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+
+    /**
+     * Time complexity O(N)
+     * Space complexity O(N)
+     */
+    private ListNode mergeTowListsWithAllocation(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode tail = head;
 
@@ -41,14 +74,5 @@ public class LC_21_MergeTwoSortedLists {
         }
 
         return head.next;
-    }
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
     }
 }
