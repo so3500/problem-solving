f660c6de861a83ced0cb24b45040c620fd15840a
so3500 <dltjddhs@gmail.com>
2021-12-12 17:25:17 +0900

    feat: add parameterized test in template source

--- src/test/kotlin/leetcode/LC_N_TitleTest.kt
+++ src/test/kotlin/leetcode/LC_N_TitleTest.kt
@@ -1,14 +1,24 @@
 package leetcode
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 
 class LC_N_TitleTest {
 	private val lcn = LC_N_Title()
 
-	@Test
-	fun test() {
-		assertThat(1).isEqualTo(1)
+	@ParameterizedTest
+	@MethodSource
+	fun test(input: Int, expected: Int) {
+		assertThat(input).isEqualTo(expected)
+	}
+
+	companion object {
+		@JvmStatic
+		private fun test() = listOf(
+			Arguments.of(1, 1)
+		)
 	}
 }
