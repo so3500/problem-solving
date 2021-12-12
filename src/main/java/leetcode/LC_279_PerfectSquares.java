c0f58015bb67c8935d2357b8feee36248631f252
so3500 <dltjddhs@gmail.com>
2021-12-12 18:42:02 +0900

    [leetcode] 279. Perfect Squares - add comment

--- src/main/java/leetcode/LC_279_PerfectSquares.java
+++ src/main/java/leetcode/LC_279_PerfectSquares.java
@@ -4,6 +4,15 @@ package leetcode;
  * related topic: Math, Dynamic Programming, Breadth-First Search
  * Time Complexity: O()
  * Space Complexity: O()
+ *
+ * dp[n]: n 을 만드는 제곱수의 최소개수
+ * dp[0] = 0
+ * dp[1] = 1
+ * dp[2] = dp[1] + 1 = 2
+ * dp[3] = dp[2] + 1 = 3
+ * dp[4] = min(dp[4-1]+1, dp[4-4]+1) = 1 (4)
+ * dp[12] = min(dp[12-1]+1, dp[12-4]+1, dp[12-9]+1) = 3 (4+4+4)
+ * dp[17] = min(dp[17-1]+1, dp[17-4]+1, dp[17-9]+1, dp[17-16]+1) = 2 (1+16)
  */
 public class LC_279_PerfectSquares {
 	public int numSquares(int n) {
