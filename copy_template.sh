da5498b85c71d75ef3d9a97ed014450b0963417e
so3500 <dltjddhs@gmail.com>
2022-02-22 22:34:07 +0900

    feat: change copy script - add another variables

--- copy_template.sh
+++ copy_template.sh
@@ -11,6 +11,16 @@ TEST_BASE='./src/test'
 #LEETCODE_TEST_BASE=$TEST_BASE/
 
 
-FILE_NAME=$1
-cp "${SRC_BASE}/java/leetcode/LC_N_Title.java" "${SRC_BASE}/java/leetcode/LC_${FILE_NAME}.java"
-cp "${TEST_BASE}/kotlin/leetcode/LC_N_Titletest.kt" "${TEST_BASE}/kotlin/leetcode/LC_${FILE_NAME}Test.kt"
+INPUT_NAME=$1
+
+CLASS_NAME=LC_${INPUT_NAME}
+TEST_CLASS_NAME=LC_${INPUT_NAME}Test
+CLASS_FILE_NAME=${CLASS_NAME}.java
+TEST_CLASS_FILE_NAME=${TEST_CLASS_NAME}.kt
+
+cp "${SRC_BASE}/java/leetcode/LC_N_Title.java" "${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}"
+cp "${TEST_BASE}/kotlin/leetcode/LC_N_TitleTest.kt" "${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}"
+
+
+#sed -i `s/LC_N_Title/${CLASS_NAME}/g` ${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}
+#sed -i `s/LC_N_TitleTest.kt/${TEST_CLASS_NAME}/g` ${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}
