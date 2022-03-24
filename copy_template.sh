0c30014e2eb933be9db0c2d6118d3e92c6f518a0
so3500 <dltjddhs@gmail.com>
2022-03-24 19:15:03 +0900

    feat: enable replace text in file

--- copy_template.sh
+++ copy_template.sh
@@ -18,9 +18,12 @@ TEST_CLASS_NAME=LC_${INPUT_NAME}Test
 CLASS_FILE_NAME=${CLASS_NAME}.java
 TEST_CLASS_FILE_NAME=${TEST_CLASS_NAME}.kt
 
-cp "${SRC_BASE}/java/leetcode/LC_N_Title.java" "${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}"
-cp "${TEST_BASE}/kotlin/leetcode/LC_N_TitleTest.kt" "${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}"
+cp ${SRC_BASE}/java/leetcode/LC_N_Title.java ${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}
+cp ${TEST_BASE}/kotlin/leetcode/LC_N_TitleTest.kt ${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}
 
+sed -i '' "s/LC_N_Title/${CLASS_NAME}/g" ${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}
+sed -i '' "s/LC_N_TitleTest/${TEST_CLASS_NAME}/g" ${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}
+sed -i '' "s/LC_N_Title/${CLASS_NAME}/g" ${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}
 
-#sed -i `s/LC_N_Title/${CLASS_NAME}/g` ${SRC_BASE}/java/leetcode/${CLASS_FILE_NAME}
-#sed -i `s/LC_N_TitleTest.kt/${TEST_CLASS_NAME}/g` ${TEST_BASE}/kotlin/leetcode/${TEST_CLASS_FILE_NAME}
+# refs
+# https://stackoverflow.com/q/21228347
