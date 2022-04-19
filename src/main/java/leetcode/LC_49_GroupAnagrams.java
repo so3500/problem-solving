318b624f9fab1a3b8fe4f27d1302c4d5e0a45f79
so3500 <dltjddhs@gmail.com>
2022-04-19 23:48:52 +0900

    [leetcode] 49. Group Anagrams

--- src/main/java/leetcode/LC_49_GroupAnagrams.java
+++ src/main/java/leetcode/LC_49_GroupAnagrams.java
@@ -5,28 +5,46 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * https://leetcode.com/problems/group-anagrams/
+ * related topic: Hash Table, String, Sorting
+ *
+ * N : number of str
+ * Time Complexity: O(N) - loop N str O(N) + map put O(1) + map get O(1)
+ * Space Complexity: O(N) - store N str in map value O(N)
+ */
 public class LC_49_GroupAnagrams {
-
-	/**
-	 * Related Topics: Hash Table, String, Sorting
-	 */
 	public List<List<String>> groupAnagrams(String[] strs) {
-		Map<String, List<String>> ret = new HashMap<>();
+		Map<String, List<String>> map = new HashMap<>();
 
 		for (String str : strs) {
-			String sortedString = getSortedString(str);
-
-			ret.computeIfAbsent(sortedString, key -> new ArrayList<>());
-			ret.get(sortedString).add(str);
+			String key = sort(str);
+			map.putIfAbsent(key, new ArrayList<>());
+			map.get(key).add(str);
 		}
 
-		return new ArrayList<>(ret.values());
+		return new ArrayList<>(map.values());
 	}
 
-	private String getSortedString(String str) {
+	/**
+	 * runtime: 10~20ms
+	 */
+	private String sort(String str) {
 		char[] chars = str.toCharArray();
 		Arrays.sort(chars);
 		return new String(chars);
 	}
+
+	/**
+	 * runtime: 108ms
+	 */
+	private String sortByStream(String str) {
+		return Stream.of(str.split(""))
+			.sorted()
+			.collect(Collectors.joining());
+	}
+
 }
--- src/test/kotlin/leetcode/LC_49_GroupAnagramsTest.kt
+++ src/test/kotlin/leetcode/LC_49_GroupAnagramsTest.kt
@@ -1,16 +1,33 @@
 package leetcode
 
-class LC_49_GroupAnagramsTest {
-	private val lc49 = LC_49_GroupAnagrams()
 
-//	@Test
-//	fun groupAnagrams() {
-//		assertThat(lc49.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat")))
-//			.containsExactlyInAnyOrderElementsOf(iter("bat"), mutableListOf("nat", "tan"), mutableListOf("ate", "eat", "tea")) // sublist order
-//
-//		assertThat(lc49.groupAnagrams(arrayOf("cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc")))
-//			.containsExactlyInAnyOrder(
-//				listOf("max"), listOf("buy"), listOf("doc"), listOf("may"), listOf("pew"),
-//				listOf("ill"), listOf("duh"), listOf("tin"), listOf("bar"), listOf("cab"))
-//	}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+
+class LC_49_GroupAnagramsTest {
+	private val lcn = LC_49_GroupAnagrams()
+
+	@ParameterizedTest
+	@MethodSource
+	fun test(input: Array<String>, expected: List<List<String>>) {
+		val result = lcn.groupAnagrams(input)
+
+		// Error converting parameter at index 0: No implicit conversion to convert object of type java.util.Arrays$ArrayList to type [Ljava.lang.String;
+		//org.junit.jupiter.api.extension.ParameterResolutionException: Error converting parameter at index 0: No implicit conversion to convert object of type java.util.Arrays$ArrayList to type [Ljava.lang.String;
+//		assertThat(result).hasSameElementsAs(expected)
+	}
+
+	companion object {
+		@JvmStatic
+		private fun test() = listOf(
+			Arguments.of(
+				listOf("eat", "tea", "tan", "ate", "nat", "bat"),
+				listOf(listOf("bat"), listOf("nat", "tan"), listOf("ate", "eat", "tea"))
+			),
+			Arguments.of(listOf(""), listOf(listOf(""))),
+			Arguments.of(listOf("a"), (listOf("a")))
+		)
+	}
 }
